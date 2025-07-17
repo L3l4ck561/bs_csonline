08529605b487359ccf817e1602b7cb24f0b364e4
l3L4ck <carlos.gabriel561btu@gmail.com>
2025-07-17 19:14:15 -0300

    save

--- curriculo-online/src/pages/home.js
+++ curriculo-online/src/pages/home.js
@@ -276,7 +276,7 @@ function App() {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="headingOne">
                                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            SagaSenai.<span className="badge bg-secondary me-1">freelancer</span> - LostFaund
+                                            SagaSenai.<span className="badge bg-secondary me-1">freelancer</span> - LostFound
                                         </button>
                                     </h2>
                                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExperiencia">
