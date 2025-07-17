f856cba53ff8859411d966d0c270e07b929896bb
l3L4ck <carlos.gabriel561btu@gmail.com>
2025-07-17 15:59:36 -0300

    save

--- curriculo-online/src/App.js
+++ curriculo-online/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Portifolio from './pages/projetos';
 import Contato from './pages/contato';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/bs_csonline">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portifolio" element={<Portifolio />} />
         <Route path="/contato" element={<Contato />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
