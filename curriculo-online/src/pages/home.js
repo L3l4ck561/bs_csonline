c5d3e731e2322fee6804dfaf4973e6eee03d8548
l3L4ck <carlos.gabriel561btu@gmail.com>
2025-07-19 11:17:08 -0300

    save

--- curriculo-online/src/pages/home.js
+++ curriculo-online/src/pages/home.js
@@ -276,7 +276,7 @@ function App() {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="headingOne">
                                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            SagaSenai.<span className="badge bg-secondary me-1">freelancer</span> - LostFound
+                                            SagaSenai - .<span className="badge bg-secondary me-1">freelancer</span>
                                         </button>
                                     </h2>
                                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExperiencia">
@@ -296,7 +296,7 @@ function App() {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="headingTwo">
                                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                            BirdGameJam.<span className="badge bg-secondary me-1">Game Jams</span> - Toca da coruja
+                                            BirdGameJam - .<span className="badge bg-secondary me-1">Game Jams</span>
                                         </button>
                                     </h2>
                                     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExperiencia">
