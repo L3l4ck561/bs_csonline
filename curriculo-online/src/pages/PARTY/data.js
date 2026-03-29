753c8ab81e180e1fedc1e2152d70ab784a34b9fd
L3l4ck561 <carlos.gabriel561btu@gmail.com>
2026-03-28 21:41:14 -0300

    add event startapday

--- curriculo-online/src/pages/PARTY/data.js
+++ curriculo-online/src/pages/PARTY/data.js
@@ -2,6 +2,7 @@ export const cartaz = [
     {coluna:'2',foto:[
         {img:process.env.PUBLIC_URL+'/certificado_premios/cangu1.png',url:process.env.PUBLIC_URL+'/certificado_premios/canguru.pdf'},
         {img:process.env.PUBLIC_URL+'/certificado_premios/cangu2.png',url:process.env.PUBLIC_URL+'/certificado_premios/oliempiadacancuru.pdf'},
+        {img:process.env.PUBLIC_URL+'/certificado_premios/startapday.png',url:process.env.PUBLIC_URL+'/certificado_premios/startapday.pdf'},
     ]}
     
 ]
