1dcf44e26f1be75603d45e7d15560ffbf7b75276
L3l4ck561 <carlos.gabriel561btu@gmail.com>
2026-04-05 23:15:49 -0300

    voltando para o home

--- curriculo-online/src/App.js
+++ curriculo-online/src/App.js
@@ -2,15 +2,16 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/HOME/main';
 import Portifolio from './pages/WORK/portifolio';
 import Contato from './pages/PARTY/certificado';
-import Contato2 from './pages/curriculo';
+import Curriculo from './pages/curriculo';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Contato2 />} />
+        <Route path="/" element={<Home />} />
         <Route path="/portifolio" element={<Portifolio />} />
         <Route path="/outros-certificados" element={<Contato />} />
+        <Route path="/curriculo-pdf" element={<Curriculo />} />
       </Routes>
     </HashRouter>
   );
